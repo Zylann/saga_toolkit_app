7c672436384f7e50ca0f1949d7c9bf98611ce714
Marc Gilleron <marc.gilleron@gmail.com>
2019-11-11 23:43:39 +0000

    Lazy-load dictionary to speed-up startup

--- words_dictionary.gd
+++ words_dictionary.gd
@@ -8,13 +8,26 @@ class Data:
 	var words = {}
 	var non_accentued_to_accentued = {}
 
-# TODO Add lazy-load/threading
 var _base_db = Data.new()
 # TODO Have ways to populate custom words
 var _custom_db = Data.new()
 
+var _queued_loadings = []
 
-func load_from_file(fpath):
+
+func load_from_file(fpath: String):
+	# Load the data only when we really need it, because it can slow down startup a lot.
+	# TODO Use threading instead, needs asynchronous usage
+	_queued_loadings.append([fpath, _base_db])
+
+
+func _dequeue_loadings():
+	for loading in _queued_loadings:
+		_load_from_file(loading[0], loading[1])
+	_queued_loadings.clear()
+
+
+static func _load_from_file(fpath, db):
 	
 	print("Loading ", fpath, "...")
 	var text = _load_text_file(fpath)
@@ -27,12 +40,14 @@ func load_from_file(fpath):
 
 	print("Registering words...")
 	for word in words_array:
-		_add_word(_base_db, word)
+		_add_word(db, word)
 	
 	print("Done")
 
 
 func exists(word):
+	_dequeue_loadings()
+	
 	for db in [_base_db, _custom_db]:
 		if db.words.has(word):
 			return true
@@ -40,6 +55,8 @@ func exists(word):
 
 
 func get_accentued_variants(non_accentued_word):
+	_dequeue_loadings()
+	
 	var all_variants = []
 	for db in [_custom_db, _base_db]:
 		if db.non_accentued_to_accentued.has(non_accentued_word):
