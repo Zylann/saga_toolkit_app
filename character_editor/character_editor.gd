d2561311f8b3eeca6fdf5daff51179886e09eb03
Marc Gilleron <marc.gilleron@gmail.com>
2019-11-24 19:17:50 +0000

    Fix word count sorting to take unused and equal characters into account

--- character_editor/character_editor.gd
+++ character_editor/character_editor.gd
@@ -39,8 +39,18 @@ func set_project(project):
 class _WordCountComparer:
 	var word_count_totals = {}
 	func compare(cname_a, cname_b):
+		var wca = -1
+		var wcb = -1
+		# It's possible the character has no occurrence
+		if word_count_totals.has(cname_a):
+			wca = word_count_totals[cname_a]
+		if word_count_totals.has(cname_b):
+			wcb = word_count_totals[cname_b]
+		# Tie break by name if word counts are equal
+		if wca == wcb:
+			return cname_a < cname_b
 		# Highest word counts come first
-		return word_count_totals[cname_a] > word_count_totals[cname_b]
+		return wca > wcb
 
 
 func _update_characters_list(project, sort_mode = -1):
