dfc60dd1ce69d747be46b8b36aa32490f6ac7848
Marc Gilleron <marc.gilleron@gmail.com>
2019-12-09 22:28:49 +0000

    Display banner from URL if it's a PNG (Godot fails to open other formats)

--- project_editor/project_editor.gd
+++ project_editor/project_editor.gd
@@ -7,8 +7,10 @@ onready var _synopsis_edit = get_node("Properties/Synopsis")
 onready var _website_edit = get_node("Properties/WebsiteURL")
 onready var _post_banner_url_edit = get_node("Properties/PostBannerURL")
 onready var _neto_saga_id_edit = get_node("Properties/NetoSagaID")
+onready var _banner = get_node("Banner")
 
 var _project : ScriptData.Project
+var _http_request : HTTPRequest = null
 
 
 func set_project(project: ScriptData.Project):
@@ -20,6 +22,43 @@ func set_project(project: ScriptData.Project):
 	_website_edit.set_url(_project.website)
 	_post_banner_url_edit.set_url(_project.post_banner_url)
 	_neto_saga_id_edit.value = _project.netophonix_saga_id
+
+	_update_banner()
+
+
+func _update_banner():
+	if _project.post_banner_url == "":
+		_banner.texture = null
+		return
+	# Create an HTTP request node and connect its completion signal.
+	if _http_request == null:
+		_http_request = HTTPRequest.new()
+		add_child(_http_request)
+		_http_request.connect("request_completed", self, "_http_request_completed")
+	
+	# Perform the HTTP request. The URL below returns a PNG image as of writing.
+	print("Requesting ", _project.post_banner_url)
+	var error = _http_request.request(_project.post_banner_url)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
+
+
+func _http_request_completed(
+		result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+		
+	if response_code != 200:
+		push_error("Request error " + str(response_code))
+		return
+	
+	var image = Image.new()
+	var error = image.load_png_from_buffer(body)
+	if error != OK:
+		push_error("Couldn't load the image.")
+		return
+		
+	var texture = ImageTexture.new()
+	texture.create_from_image(image)
+	_banner.texture = texture
 
 
 func _on_Title_text_changed(new_text):
@@ -40,6 +79,7 @@ func _on_NetoSagaID_value_changed(value):
 func _on_PostBannerURL_changed(new_url):
 	_project.post_banner_url = new_url.strip_edges()
 	_project.make_modified()
+	_update_banner()
 
 
 func _on_WebsiteURL_changed(new_url):
